d58e811afd4c4bd7f51d8383120deab95752db44
eabeda <elyes.abeda@gmail.com>
2017-07-07 15:37:18 +0200

    Fix build.sh

--- build.sh
+++ build.sh
@@ -3,15 +3,16 @@
 export PATH=~/git/testOpenEmbedded/bitbake/bin:$PATH
 cd bitbake/
 tree -L 2
-mkdir hello/
+mkdir -p hello/
 cd hello
-mkdir conf
-cp ../files/bblayers.conf conf/bitbake.conf
-cp ../files/bblayers.conf conf/bblayers.conf
+mkdir -p conf
+cp ../../files/bitbake.conf conf/bitbake.conf
+cp ../../files/bblayers.conf conf/bblayers.conf
 mkdir -p mylayer/conf/
-cp ../files/layer.conf mylayer/conf/layer.conf
+cp ../../files/layer.conf mylayer/conf/layer.conf
 mkdir -p mylayer/classes/
-cp ../files/base.bbclass mylayer/classes/base.bbclass
-cp ../files/base.bbclass mylayer/printhello.bb
+cp ../../files/base.bbclass mylayer/classes/base.bbclass
+cp ../../files/printhello.bb mylayer/printhello.bb
+bitbake world
 bitbake printhello -D
 
